5704c1c6881c43afd0f7e1ca365afa751f9e31c0
cjy7 <1352481068@qq.com>
2019-09-13 17:09:57 +0800

    product

---
+++ src/components/search.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="pro-top">
+    <div class="pro-top-search">
+         <span class="pro-search-sp1"><img src="../assets/img/suosou.png" alt="" class="pro-search-img1">
+         </span>
+        <span> <input type="text"      class="search-input" placeholder="搜索餐厅">
+           </span>
+        <span>
+              <img src="../assets/img/03.png" alt="" class="pro-search-img2">
+              <p class="pro-p-map">地图</p>
+          </span>
+        </div>
+    </div>
+</template>
