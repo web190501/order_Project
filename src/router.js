59585fd241360c4711fad3d1d1dabd8e7e9858ea
hanshaomin <1141558294@qq.com>
2019-09-13 17:40:32 +0800

    details

--- src/router.js
+++ src/router.js
@@ -1,23 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
-
+import details from "./views/details.vue"
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: Home
+      path:'/details',component:details,
     },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // whih is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
   ]
 })
---
+++ src/views/details.vue
@@ -0,0 +1,52 @@
+<template>
+    <div>
+        <div>
+            <img id="details_img_d1" class="details_img" src="../../public/img/01aa.jpg" alt="">
+            <img id="details_img_d2"  class="details_img" src="../../public/img/6fcd36ea9be3dbce429202f2757123bc.jpg" alt="">
+            <img id="details_img_d3"  class="details_img" src="../../public/img/01aa.jpg" alt="">
+        </div>
+        <div id="details_resc">
+            <p id="details_title">都市晚餐</p>
+            <div>
+                <button id="details_btn1">
+                    套餐
+                </button>
+                <span id="details_span1">￥698</span>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+
+</script>
+<style>
+#details_span1{
+    color:#dc2224;
+    font-size:1.5625rem;
+}
+#details_btn1{
+    color:#95989a;
+    border:0.0625rem solid #95989a;
+    border-radius:0.25rem;
+    padding: 0.125rem 0.4375rem;
+    margin-right: 0.9375rem;
+
+}
+#details_title{
+    font-size: 1.5625rem;
+    color:#333;
+    font-weight:600;
+    line-height: 1.2
+}
+.details_img{
+    width:100%;
+    /* width:20.625rem; */
+    height:13.75rem;
+}
+#details_img_d1{
+    display:none;
+}
+#details_img_d2{
+    display: none
+}
+</style>
