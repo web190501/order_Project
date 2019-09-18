e9dd3174bd90c324b88cc9eb7d5a957d97679259
hanshaomin <1141558294@qq.com>
2019-09-18 11:04:00 +0800

    修改

--- babel.config.js
+++ babel.config.js
@@ -2,11 +2,4 @@ module.exports = {
   presets: [
     '@vue/app'
   ],
-  plugins: [
-    ['import', {
-      libraryName: 'vant',
-      libraryDirectory: 'es',
-      style: true
-    }, 'vant']
-  ]
 }
